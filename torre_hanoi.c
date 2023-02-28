3c837c8cf753db209b9a6c5fa936c4e7e064339a
Augusto Santos <111477028+AugustoCSantos@users.noreply.github.com>
2023-02-27 22:52:13 -0300

    Create torre_hanoi.c

---
+++ torre_hanoi.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+void mover_disco(int n, char origem, char destino, char aux);
+
+int main()
+{
+    int discos;
+
+    scanf("%d", &discos);
+
+    mover_disco(discos, '1', '2', '3');
+   
+    return 0;
+}
+
+void mover_disco(int n, char origem, char destino, char aux)
+{
+    if(d == 1)
+    {
+        printf("%c %c\n", origem, aux);
+    }
+    else
+    {
+        mover_disco(d - 1, origem, aux, destino);
+
+        printf("%c %c\n", origem, aux);
+
+        mover_disco(d - 1, destino, origem, aux);
+    }
+    
+}
